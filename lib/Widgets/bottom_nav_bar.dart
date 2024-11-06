237c66698e433572d2b8e9ebb0e830989922d8d9
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-07 02:13:21 +0600

    bottomnavbar

---
+++ lib/Widgets/bottom_nav_bar.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(primarySwatch: Colors.deepOrange),
+      themeMode: ThemeMode
+          .system, // Toggles between light and dark theme based on system settings
+      home: const HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  MySnackbar(message, context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My App Bar'),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+        toolbarHeight: 50,
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        actions: [
+          IconButton(
+              onPressed: () {
+                MySnackbar("Search", context);
+              },
+              icon: const Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                MySnackbar("Email", context);
+              },
+              icon: const Icon(Icons.email)),
+          IconButton(
+              onPressed: () {
+                MySnackbar("Comment", context);
+              },
+              icon: const Icon(Icons.comment)),
+          IconButton(
+              onPressed: () {
+                MySnackbar("Settings", context);
+              },
+              icon: const Icon(Icons.settings)),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+        foregroundColor: Colors.white,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: 0,
+        backgroundColor: Colors.amber,
+        onDestinationSelected: (int selectedIndex) {},
+        destinations: [
+          NavigationDestination(
+            icon: Icon(
+              Icons.home,
+              color: Colors.blue,
+            ),
+            label: "home",
+          ),
+          NavigationDestination(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.blue,
+              ),
+              label: "Settings"),
+          NavigationDestination(
+              icon: Icon(
+                Icons.search,
+                color: Colors.blue,
+              ),
+              label: "Search"),
+        ],
+      ),
+      body: const Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
