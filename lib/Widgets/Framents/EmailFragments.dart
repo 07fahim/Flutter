9839e46c5a0beea978fc28aa366cbca31f912d83
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 01:00:00 +0600

    Contacts Fragment

---
+++ lib/Widgets/Framents/EmailFragments.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Emailfragments extends StatelessWidget {
+  const Emailfragments({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Email Screen")),
+    );
+  }
+}
