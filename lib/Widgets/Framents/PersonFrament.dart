41d475bf03e449d9a3cd63af14c011fb3b14eab2
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 00:57:46 +0600

    Person Fragment

---
+++ lib/Widgets/Framents/PersonFrament.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Personframent extends StatelessWidget {
+  const Personframent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Person Screen")),
+    );
+  }
+}
