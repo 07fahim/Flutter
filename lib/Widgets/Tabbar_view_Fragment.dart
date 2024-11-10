a66b05c21d9f4486d02b9ae2c410c071d5201c8b
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 00:37:22 +0600

    TabBar

--- lib/Widgets/Tabbar_view_Fragment.dart
+++ lib/Widgets/Tabbar_view_Fragment.dart
@@ -11,9 +11,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      darkTheme: ThemeData(primarySwatch: Colors.deepOrange),
-      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[200],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.lightBlueAccent,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        scaffoldBackgroundColor: Colors.grey[900],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepOrange,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      themeMode: ThemeMode.light, // Try ThemeMode.dark to test dark theme
       home: const HomeActivity(),
     );
   }
@@ -30,46 +44,47 @@ class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 7,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("My Appbar"),
-            backgroundColor: Colors.lightBlueAccent,
-            bottom: const TabBar(
-                isScrollable: true,
-                indicatorPadding: EdgeInsets.zero,
-                padding: EdgeInsets.zero,
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.home),
-                    text: "Home",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.person),
-                    text: "person",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.settings),
-                    text: "Settings",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.contacts),
-                    text: "Contacts",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.email),
-                    text: "Email",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.search),
-                    text: "Search",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.balance),
-                    text: "Balance",
-                  ),
-                ]),
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("My Appbar"),
+          bottom: const TabBar(
+            isScrollable: true,
+            indicatorPadding: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: "Home",
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                text: "person",
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+                text: "Settings",
+              ),
+              Tab(
+                icon: Icon(Icons.contacts),
+                text: "Contacts",
+              ),
+              Tab(
+                icon: Icon(Icons.email),
+                text: "Email",
+              ),
+              Tab(
+                icon: Icon(Icons.search),
+                text: "Search",
+              ),
+              Tab(
+                icon: Icon(Icons.balance),
+                text: "Balance",
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
