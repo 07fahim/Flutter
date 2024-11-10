e818e5c4f79e8af7ac294467e40d05b01924cdd4
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 01:01:37 +0600

    Search Fragment

---
+++ lib/Widgets/Framents/SearchFraments.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Searchframents extends StatelessWidget {
+  const Searchframents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Search Screen")),
+    );
+  }
+}
