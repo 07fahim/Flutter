01b202a7861c963f6e0c6075fb110bce107de781
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 00:59:21 +0600

    Contacts Fragment

---
+++ lib/Widgets/Framents/ContactsFraments.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Contactsframents extends StatelessWidget {
+  const Contactsframents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Contacts Screen")),
+    );
+  }
+}
