7d66137ead5abc5c2aa77d4698dda4daf299ebd7
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 01:02:14 +0600

    Balance  Fragment

---
+++ lib/Widgets/Framents/BalanceFraments.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Balanceframents extends StatelessWidget {
+  const Balanceframents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Balance Screen")),
+    );
+  }
+}
