7cf626b07cbe6fc4e26f92ba013e100154ae5a99
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-23 00:45:08 +0600

    Fractional Sizebox

---
+++ lib/Widgets/Fraction_size_box.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(primarySwatch: Colors.deepOrange),
+      themeMode: ThemeMode
+          .system, // Toggles between light and dark theme based on system settings
+      home: const HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  MySnackbar(message, context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My App Bar'),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+          toolbarHeight: 50,
+          elevation: 0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Search", context);
+                },
+                icon: const Icon(Icons.search)),
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Email", context);
+                },
+                icon: const Icon(Icons.email)),
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Comment", context);
+                },
+                icon: const Icon(Icons.comment)),
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Settings", context);
+                },
+                icon: const Icon(Icons.settings)),
+          ],
+        ),
+        body: Center(
+          child: FractionallySizedBox(
+            widthFactor: 0.6,
+            heightFactor: 0.3,
+            child: Container(
+              child: Text("Fahim"),
+              alignment: Alignment.center,
+              color: Colors.amber,
+            ),
+          ),
+        ));
+  }
+}
