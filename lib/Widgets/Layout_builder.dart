1227f0612074655f483a209c6720c31ccb96df1c
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-23 00:44:03 +0600

    LayoutBuilder

---
+++ lib/Widgets/Layout_builder.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: non_constant_identifier_names, file_names
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(primarySwatch: Colors.deepOrange),
+      themeMode: ThemeMode
+          .system, // Toggles between light and dark theme based on system settings
+      home: const HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  MySnackbar(message, context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My App Bar'),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+          toolbarHeight: 50,
+          elevation: 0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Search", context);
+                },
+                icon: const Icon(Icons.search)),
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Email", context);
+                },
+                icon: const Icon(Icons.email)),
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Comment", context);
+                },
+                icon: const Icon(Icons.comment)),
+            IconButton(
+                onPressed: () {
+                  MySnackbar("Settings", context);
+                },
+                icon: const Icon(Icons.settings)),
+          ],
+        ),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 600) {
+            return Container(
+              width: 400,
+              height: 400,
+              color: Colors.red,
+            );
+          } else {
+            return Container(
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+            );
+          }
+        }));
+  }
+}
